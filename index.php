0217eb1cb0d3289fab8b95671e2fc8d35976f419
nihalZett <nihal.web.dev@gmail.com>
2026-06-23 19:37:53 +0200

    change index.html to index.php to write php logic

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Prisma Residences</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<header>Prisma Residences</header>		
+	<nav>
+		<a class="navBar-element" href="">HOME</a>
+		<a class="navBar-element" href="properties.html">PROPERTIES</a>
+		<a class="navBar-element" href="sell_with_us.html">SELL WITH US</a>
+		<a class="navBar-element" href="contact_us.html">CONTACT US</a>
+		<a class="navBar-element" href="about_us.html">ABOUT US</a>
+	</nav>
+	<main>
+		<div class="hero-image">
+			<div class="hero-text">
+				<h1>What are you looking for?</h1>	
+				<form method="get" action="">
+					<label for="property-type">Property type</label>
+					<select id="property-type" name="property-type">
+						<option value="villa">Villa</option>
+						<option value="mansion">Mansion</option>
+						<option value="penthouse">Penthouse</option>
+					</select>
+
+					<label for="location">Location</label>
+					<select id="location" name="location">
+						<option value="marbella">Marbella</option>
+						<option value="madrid">Madrid</option>
+						<option value="costa-del-sol">Costa del Sol</option>
+						<option value="mallorca">Mallorca</option>
+						<option value="ibiza">Ibiza</option>
+					</select>
+
+					<button name="search-btn" type="submit" value="search">Search properties</button>
+				</form>
+			</div>
+		</div>
+	<section> 
+		<h1>FEATURED PROPERTIES</h1>
+		<article>
+			<div>
+				<h1>This is a property</h1>
+				<figure>
+  					<img src="" alt="Can't load the image.">
+  					<figcaption>this is the caption of the image.</figcaption>
+				</figure>
+			</div>
+		</article>
+	</section>
+	</main>
+	<footer>
+		<p>&copy; 2026 Prisma Residences. Todos los derechos están reservados. 
+		</p>
+	</footer>
+</body>
+</html>
